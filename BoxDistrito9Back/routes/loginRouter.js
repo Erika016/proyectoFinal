0cb1a78c4197445572f8661063f7c8f47c24147c
erika <ericadominguezbarrientos@gmail.com>
2022-07-09 19:08:34 +0200

    Todo eliminado

--- BoxDistrito9Back/routes/loginRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controlador = require('../controllers/loginController');
-
-// realizar el login
-router.post('/login', controlador.login);
-
-module.exports = router;
--- BoxDistrito9front/src/components/spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Spinner.module.css";
-
-export function Spinner() {
-  return (
-    <>
-      <div className={styles.ldsHourglass}></div>
-    </>
-  );
-}
